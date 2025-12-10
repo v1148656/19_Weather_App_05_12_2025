bb29e364cc0fd0654745770a0f186a6c59a36dc2
v1148656 <v1148656@gmail.com>
2025-12-10 18:48:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const API_KEY = "fa1e72ff893c6a4a5ed4077327e855b4";
+const cityValueInput = document.getElementById("cityValueInput");
+const weatherBtn = document.getElementById("weatherBtn");
+const weatherInfo = document.getElementById("weatherInfo");
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("weather")) {
+    const data = JSON.parse(localStorage.getItem("weather"));
+    displayWeather(data);
+  }
+});
+
+async function fetchWeather(url) {
+  try {
+    weatherInfo.innerHTML = `<img src="./Image20251208200339.gif" alt="loading" />`;
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(res.status);
+    const data = await res.json();
+    localStorage.setItem("weather", JSON.stringify(data));
+    return data;
+  } catch (error) {
+    weatherInfo.innerHTML = `<p>Не удалось получить геолокацию</p>`;
+  }
+}
+
+weatherBtn.onclick = async () => {
+  const city = cityValueInput.value.trim();
+  const lang = document.getElementById("langSelect").value;
+  displayWeather(
+    await fetchWeather(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=${lang}`
+    )
+  );
+};
+
+const geoWeatherBtn = document.getElementById("geoWeatherBtn");
+
+geoWeatherBtn.onclick = async () => {
+  try {
+    // 1. Получаем координаты
+    const position = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+    const lang = document.getElementById("langSelect").value;
+    const { latitude, longitude } = position.coords;
+    displayWeather(
+      await fetchWeather(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=${lang}`
+      )
+    );
+  } catch (err) {
+    weatherInfo.innerHTML = `<p>Не удалось получить геолокацию</p>`;
+  }
+};
+
+function displayWeather({
+  name,
+  weather: [{ icon, description }],
+  main: { temp, feels_like, humidity },
+  wind: { speed },
+}) {
+  cityValueInput.value = "";
+  weatherInfo.innerHTML = `
+        <p style="text-transform: uppercase;">${name}</p>
+        <img class="weather-icon" src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="iconWeatherDescription">
+        <p><strong>Температура: </strong>${Math.round(temp)}°C</p>
+        <small><strong>Ощущается как: </strong>${Math.round(feels_like)}°C</small>
+        <p><strong>Описание: </strong>${description}</p>
+        <p><strong>Влажность:</strong> ${humidity}%</p>
+        <p><strong>Скорость ветра:</strong> ${speed} m/s</p>`;
+}
